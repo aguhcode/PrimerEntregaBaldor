b5dbc046316062a08e7575853a8f7b705220b5da
aguh <aguhwtf1@gmail.com>
2024-05-16 17:43:10 -0300

    1er commit

---
+++ agu.js
@@ -0,0 +1,14 @@
+
+var edad = prompt("Por favor, ingresa tu edad:"); //prompt
+
+  if (edad >= 18) {         //operador
+    console.log("Puedes votar en las proximas elecciones!");
+} else {
+    console.log("Debes tener al menos 18 anios para votar.");
+}
+
+
+//console.log("Imprimiendo números del 1 al 10:");  //ciclo for
+//for (var i = 1; i <= 10; i++) {
+//    console.log(i);
+//}
