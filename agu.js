3b6e0d93f24ccc8a27c0f5b662180c562112861b
aguh <aguhwtf1@gmail.com>
2024-05-20 17:28:01 -0300

    funct2.0 fix

--- agu.js
+++ agu.js
@@ -1,13 +1,19 @@
-let edad;
+function verificarEdadParaVotar() {
+  let edad;
 
-do {
-  edad = prompt("Por favor, ingresa tu edad:");
-} while (isNaN(edad) || edad === "");
+  do {
+    edad = prompt("Por favor, ingresa tu edad:");
+  } while (isNaN(edad) || edad === "");
 
-edad = parseInt(edad);
+  edad = parseInt(edad);
 
-if (edad >= 18) {
-  console.log("Podes votar en las proximas elecciones!");
-} else {
-  console.log("Debes tener al menos 18 anios para votar.");
+
+  if (edad >= 18) {
+    console.log("Podes votar en las proximas elecciones!");
+  } else {
+    console.log("Debes tener al menos 18 anios para votar.");
+  }
 }
+
+
+verificarEdadParaVotar();
