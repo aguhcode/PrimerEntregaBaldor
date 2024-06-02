1b17ff8f42820928847e742c20e987be022ee5bf
aguh <aguhwtf1@gmail.com>
2024-06-02 13:13:34 -0300

    .fx

--- agu.js
+++ agu.js
@@ -27,7 +27,6 @@ function verificarEdadesParaVotar() {
 
 verificarEdadesParaVotar();
 
-// Objeto para representar una reserva de hotel
 class Reserva {
   constructor(nombre, edad, numeroHabitacion, noches) {
     this.nombre = nombre;
@@ -36,17 +35,14 @@ class Reserva {
     this.noches = noches;
   }
 
-  // Metodo para calcular el costo de la reserva
   calcularCosto(precioPorNoche) {
     return this.noches * precioPorNoche;
   }
 }
 
-// Variables necesarias
 const precioPorNoche = 100;
 let reservas = [];
 
-// Funcion para solicitar una reserva
 function solicitarReserva() {
   let continuar = true;
 
@@ -56,28 +52,24 @@ function solicitarReserva() {
     let noches;
     let numeroHabitacion;
 
-    // Validar la edad
     do {
       edad = prompt("Por favor, ingresa tu edad (debe ser un número válido):");
     } while (isNaN(edad) || edad.trim() === "");
 
     edad = parseInt(edad);
 
-    // Validar el numero de noches
     do {
       noches = prompt("¿Cuántas noches deseas reservar?");
     } while (isNaN(noches) || noches.trim() === "");
 
     noches = parseInt(noches);
 
-    // Validar el numero de habitación
     do {
       numeroHabitacion = prompt("Por favor, ingresa el número de habitación (debe ser un número válido):");
     } while (isNaN(numeroHabitacion) || numeroHabitacion.trim() === "");
 
     numeroHabitacion = parseInt(numeroHabitacion);
 
-    // Verificar la edad para permitir la reserva
     if (edad >= 18) {
       let nuevaReserva = new Reserva(nombre, edad, numeroHabitacion, noches);
       reservas.push(nuevaReserva);
@@ -86,15 +78,12 @@ function solicitarReserva() {
       alert("Debes tener al menos 18 años para hacer una reserva.");
     }
 
-    // Preguntar al usuario si quiere hacer otra reserva
     continuar = confirm("¿Quieres hacer otra reserva?");
   }
 
-  // Mostrar todas las reservas
   mostrarReservas();
 }
 
-// Funcion para mostrar todas las reservas
 function mostrarReservas() {
   console.log("Reservas realizadas:");
   reservas.forEach((reserva, index) => {
@@ -102,20 +91,16 @@ function mostrarReservas() {
   });
 }
 
-// Funcion para buscar una reserva por nombre
 function buscarReservaPorNombre(nombre) {
   return reservas.filter(reserva => reserva.nombre.toLowerCase() === nombre.toLowerCase());
 }
 
-// Funcion para buscar reservas por número de habitación
 function buscarReservasPorNumeroHabitacion(numeroHabitacion) {
   return reservas.filter(reserva => reserva.numeroHabitacion === numeroHabitacion);
 }
 
-// Llamada a la funcion para solicitar reservas
 solicitarReserva();
 
-// Ejemplos de uso de los métodos de búsqueda y filtrado
 let nombreBusqueda = prompt("Ingresa el nombre para buscar una reserva:");
 let reservasEncontradas = buscarReservaPorNombre(nombreBusqueda);
 if (reservasEncontradas.length > 0) {
